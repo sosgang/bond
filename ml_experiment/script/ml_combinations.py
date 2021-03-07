5e383ae317632b7a9df640cc989130b4df275398
fpoggi <francesco.poggi5@unibo.it>
2021-03-07 15:44:27 +0100

    ML experiment: updated code (ray)

--- ml_experiment/script/ml_combinations.py
+++ ml_experiment/script/ml_combinations.py
@@ -2,17 +2,17 @@ import time
 import sys
 import pandas as pd   
 import numpy as np
-#import matplotlib.pyplot as plt
 from sklearn import svm, tree
 from sklearn.model_selection import cross_validate
 from sklearn.metrics import *
 from sklearn.utils import resample
 from itertools import combinations
-import mylib
+import ray
 
 sections = ["AP", "FP"]
 fields = ["10-G1", "13-D4"]
 coverages = ["A","AB","ABC"]
+classifierTypes = ["svm_1","svm_0.5","svm_0.1","svm_0.02","decisionTree"]
 features= ['cand', 'co-au',
 			'cand_comm', 'comm_cand',
 			'BC', 'CC',
@@ -24,6 +24,100 @@ inputFile = "../../complete_metrics.csv"
 outputFile_Partial = "../data/combinations_%d_features.csv"
 outputFile_Total = "../data/combinations_ALL_features.csv"
 outputFile_F1 = "../data/combinations_F1_0.7.csv"
+
+cpus = 4
+ray.init(num_cpus=cpus, ignore_reinit_error=True)
+
+
+def balanceSample(data):
+	countSi = data[data.outcome == "Si"].shape[0]
+	countNo = data[data.outcome == "No"].shape[0]
+	if countSi > countNo:
+		# resample Sis with stratification
+		data_resampled = resample(data[data.outcome == "No"], 
+											replace=True,			# sample with replacement
+											n_samples=countSi,		# to match majority class
+											random_state=123,		# reproducible results
+											stratify=data[data.outcome == "No"])
+		return pd.concat([data[data.outcome == "Si"], data_resampled])
+		
+	elif countSi < countNo:
+		# resample Nos with stratification
+		data_resampled = resample(data[data.outcome == "Si"], 
+											replace=True,			# sample with replacement
+											n_samples=countNo,		# to match majority class
+											random_state=123,		# reproducible results
+											stratify=data[data.outcome == "Si"])
+		return pd.concat([data[data.outcome == "No"], data_resampled])
+	return data
+
+
+def isDivergent(y_pred):
+	if y_pred.count("Si") == 0 or y_pred.count("No") == 0:
+		return True
+	return False
+
+
+@ray.remote
+def incremental(df,field,section,features,classifierType,coverage=["A"],doBalance=True):
+	
+	if classifierType not in ["svm_1","svm_0.5","svm_0.1","svm_0.02","decisionTree"]:
+		print ("ERROR: classifierType should be 'svm' or 'decisionTree' - %s provided." % classifierType)
+		sys.exit()
+	
+	# filter field, section and coverage, and remove NaN values
+	temp = df[(df["field"] == field) & (df["role"] == section)]
+	df_filtered = temp[temp["coverage"].isin(coverage)]
+	
+	df_TrainSet = df_filtered[df_filtered["term"].isin(list(range(1,5)))][features+["outcome"]].dropna()
+	df_TestSet = df_filtered[df_filtered["term"].isin(list(range(5,6)))][features+["outcome"]].dropna()
+		
+	# Save this info before resampling
+	res = dict()
+	res["train (Si/No)"] = "%d (%d/%d)" % (df_TrainSet.shape[0], df_TrainSet[df_TrainSet.outcome == "Si"].shape[0], df_TrainSet[df_TrainSet.outcome == "No"].shape[0])
+	res["test (Si/No)"] = "%d (%d/%d)" % (df_TestSet.shape[0], df_TestSet[df_TestSet.outcome == "Si"].shape[0], df_TestSet[df_TestSet.outcome == "No"].shape[0])
+		
+	# Resample test
+	if doBalance:
+		df_TrainSet = balanceSample(df_TrainSet)
+	
+	if df_TrainSet.shape[0] == 0 or df_TestSet.shape[0] == 0:
+		print ("\tSKIP.")
+		return
+		
+	if classifierType == "svm_1":
+		clf = svm.SVC(kernel='linear', C=1, random_state=0)
+	elif classifierType == "svm_0.5":
+		clf = svm.SVC(kernel='linear', C=0.5, random_state=0)
+	elif classifierType == "svm_0.1":
+		clf = svm.SVC(kernel='linear', C=0.1, random_state=0)
+	elif classifierType == "svm_0.02":
+		clf = svm.SVC(kernel='linear', C=0.02, random_state=0)
+	elif classifierType == "decisionTree":
+		clf = tree.DecisionTreeClassifier(random_state=0)
+		
+	clf = clf.fit(df_TrainSet[features], df_TrainSet["outcome"])
+		
+	y_true = df_TestSet["outcome"].tolist()
+	y_pred = clf.predict(df_TestSet[features]).tolist()
+		
+	if isDivergent(y_pred):
+		res["divergent"] = True
+	else:
+		res = {**res,**classification_report(y_true, y_pred,output_dict=True)["weighted avg"]}
+		res["precision"] = round(res["precision"],3)
+		res["recall"] = round(res["recall"],3)
+		res["f1-score"] = round(res["f1-score"],3)
+		res["divergent"] = False
+		res["pred Si/No"] = "(%d/%d)" % (y_pred.count("Si"), y_pred.count("No"))
+	res["field"] = field
+	res["role"] = section
+	res["coverage"] = "".join(coverage)
+	res["classifier"] = classifierType
+	res["num-features"] = len(features)
+	
+	return {**res, **featureListToDict(features)}
+
 
 def featureListToDict(features):
 	res = dict()
@@ -46,79 +140,45 @@ def featureListToDict(features):
 
 
 df = pd.read_csv(inputFile)
-counter = 0
+
 for i in range(1,len(features)+1):
-	res = pd.DataFrame(columns=["field","role","coverage","classifier","divergent","precision","recall","f1-score","train (Si/No)","test (Si/No)", "pred Si/No",
-								"index-feat.comb.", "num-features", "cand", "co-au","cand_comm","comm_cand","BC", "CC","cand_other","other_cand","books", 
-								"articles","other_pubbs","nd_m1","nd_m2","nd_m3"
-								])
+	start_time = time.time()
+	counter = 0
+	ray_results = list()
 	for combination in list(combinations(features,i)):
-		print ("%d) %d" % (i, counter))
-		counter += 1
 		featureList = list(combination)
-		for field in ["10-G1", "13-D4"]:
+		for field in fields:
 			for section in sections:
 				for coverage in coverages:
-					for classifierType in ["svm_1","svm_0.5","svm_0.1","svm_0.02","decisionTree"]:
-						resMetrics = mylib.incremental(df,field,section,featureList,classifierType,list(coverage),True)
-						
-						if resMetrics["divergent"]:
-							print ("\t%s %s %s %s - divergent" % (field, section, coverage, classifierType))
-							d = dict()
-							d["field"] = field
-							d["role"] = section
-							d["coverage"] = coverage
-							d["classifier"] = classifierType
-							tempTrain = filterDF(df,field,section,list(range(1,5)),featureList,list(coverage))
-							d["train (Si/No)"] = "%d (%d/%d)" % (tempTrain.shape[0],tempTrain[tempTrain.outcome == "Si"].shape[0],tempTrain[tempTrain.outcome == "No"].shape[0])
-							tempTest = filterDF(df,field,section,[5],featureList,list(coverage))
-							d["test (Si/No)"] = "%d (%d/%d)" % (tempTest.shape[0],tempTest[tempTest.outcome == "Si"].shape[0],tempTest[tempTest.outcome == "No"].shape[0]) #getSiNo(df,field,section,[5],featureList,list(coverage))
-							d["divergent"] = True
-							d["index-feat.comb."] = counter
-							d["num-features"] = i
-							f = featureListToDict(featureList)
-							d = {**d, **f}
-							res = res.append(d, ignore_index=True)
-							continue
-						
-						d = resMetrics["report"]["weighted avg"]
-						d["field"] = field
-						d["role"] = section
-						d["coverage"] = coverage
-						d["classifier"] = classifierType
-						tempTrain = filterDF(df,field,section,list(range(1,5)),featureList,list(coverage))
-						d["train (Si/No)"] = "%d (%d/%d)" % (tempTrain.shape[0],tempTrain[tempTrain.outcome == "Si"].shape[0],tempTrain[tempTrain.outcome == "No"].shape[0])
-						tempTest = filterDF(df,field,section,[5],featureList,list(coverage))
-						d["test (Si/No)"] = "%d (%d/%d)" % (tempTest.shape[0],tempTest[tempTest.outcome == "Si"].shape[0],tempTest[tempTest.outcome == "No"].shape[0]) #getSiNo(df,field,section,[5],featureList,list(coverage))
-						d["pred Si/No"] = "(%d/%d)" % (resMetrics["y_pred"].count("Si"), resMetrics["y_pred"].count("No"))
-						d["divergent"] = False
-						d["index-feat.comb."] = counter
-						d["num-features"] = i
-						f = featureListToDict(featureList)
-						d = {**d, **f}
-						
-						res = res.append(d, ignore_index=True)
-						continue
-	# save results computed using i feature/s
-	res.to_csv(outputFile_Partial % i)
+					for classifierType in classifierTypes:
+						ray_results.append(incremental.remote(df,field,section,featureList,classifierType,list(coverage),True))
+						counter += 1
+	
+	duration1 = time.time() - start_time
+	print ('Executing the for loop # ' + str(i) + ' took {:.3f} seconds.'.format(duration1))
+	print ('Computing ' + str(counter) + ' models...')
+	
+	dictResultsModels = ray.get(ray_results)
+	duration2 = time.time() - start_time
+	print ('Executing the WHOLE COMPUTATION for loop #' + str(i) + ' took {:.3f} seconds.'.format(duration2))
+	
+	columnsOrdered = ["field","role","coverage","classifier","divergent","precision","recall","f1-score","train (Si/No)","test (Si/No)", "pred Si/No",
+		"num-features", "cand", "co-au","cand_comm","comm_cand","BC", "CC","cand_other","other_cand","books", 
+		"articles","other_pubbs","nd_m1","nd_m2","nd_m3", 'support'
+	]
+	results_iteration_df = pd.DataFrame(dictResultsModels).reindex(columns=columnsOrdered,copy=False)
+	results_iteration_df.to_csv(outputFile_Partial % i, index=False)
 
 
-# concatenate all results in a single file
+# concatenate all results and save to single file
 for i in range(1,len(features)+1):
 	df = pd.read_csv(outputFile_Partial % i)
 	if i == 1:
-		res = df
+		results_total_df = df
 	else:
-		res = res.append(df)
-res.to_csv(outputFile_Total)
+		results_total_df = results_total_df.append(df)
+results_total_df.to_csv(outputFile_Total, index=False)
 
-
-# concatenate results having f1-score >= 0.700 in a single file
-for i in range(1,len(features)+1):
-	df = pd.read_csv(outputFile_Partial % i)
-	if i == 1:
-		res = df[(df["f1-score"] >= 0.7)]
-	else:
-		res = res.append(df[(df["f1-score"] >= 0.7)])	
-res.to_csv(outputFile_F1)
-
+# save results having f1-score >= 0.700 to file
+best_results = results_total_df[(results_total_df["f1-score"] >= 0.7)]
+best_results.to_csv(outputFile_F1, index=False)
--- ml_experiment/script/plotDecisionTree.py
+++ ml_experiment/script/plotDecisionTree.py
@@ -8,65 +8,90 @@ from sklearn.model_selection import cross_validate
 from sklearn.metrics import *
 from sklearn.utils import resample
 from itertools import combinations
-import mylib
+
 
 inputFile = "../../complete_metrics.csv"
+
+def balanceSample(data):
+	countSi = data[data.outcome == "Si"].shape[0]
+	countNo = data[data.outcome == "No"].shape[0]
+	if countSi > countNo:
+		# resample Sis with stratification
+		data_resampled = resample(data[data.outcome == "No"], 
+											replace=True,			# sample with replacement
+											n_samples=countSi,		# to match majority class
+											random_state=123,		# reproducible results
+											stratify=data[data.outcome == "No"])
+		return pd.concat([data[data.outcome == "Si"], data_resampled])
+		
+	elif countSi < countNo:
+		# resample Nos with stratification
+		data_resampled = resample(data[data.outcome == "Si"], 
+											replace=True,			# sample with replacement
+											n_samples=countNo,		# to match majority class
+											random_state=123,		# reproducible results
+											stratify=data[data.outcome == "Si"])
+		return pd.concat([data[data.outcome == "No"], data_resampled])
+	return data
+
+
+def isDivergent(y_pred):
+	if y_pred.count("Si") == 0 or y_pred.count("No") == 0:
+		return True
+	return False
+
 
 def myIncremental(df,field,section,features,classifierType,coverage=["A"],doBalance=True):
 	
 	if classifierType not in ["svm_1","svm_0.5","svm_0.1","svm_0.02","decisionTree"]:
 		print ("ERROR: classifierType should be 'svm' or 'decisionTree' - %s provided." % classifierType)
 		sys.exit()
-	
 		
 	# filter field, section and coverage, and remove NaN values
 	temp = df[(df["field"] == field) & (df["role"] == section)]
 	df_filtered = temp[temp["coverage"].isin(coverage)]
 	
-	for i in range(5,6):
-		terms = list(range(1,i))
-		df_TrainSet = df_filtered[df_filtered["term"].isin(terms)][features+["outcome"]].dropna()
-		df_TestSet = df_filtered[df_filtered["term"].isin(list(range(i,6)))][features+["outcome"]].dropna()
+	df_TrainSet = df_filtered[df_filtered["term"].isin(list(range(1,5)))][features+["outcome"]].dropna()
+	df_TestSet = df_filtered[df_filtered["term"].isin([5])][features+["outcome"]].dropna()
 		
-		# RESAMPLE TEST
-		if doBalance:
-			df_TrainSet = mylib.balanceSample(df_TrainSet)
+	# RESAMPLE TEST
+	if doBalance:
+		df_TrainSet = balanceSample(df_TrainSet)
 		
-		if df_TrainSet.shape[0] == 0 or df_TestSet.shape[0] == 0:
-			print ("\tSKIP.")
-			continue
+	if df_TrainSet.shape[0] == 0 or df_TestSet.shape[0] == 0:
+		print ("\tSKIP.")
+		return
 		
-		##scoring = ['precision_macro', 'recall_macro', 'f1_macro']
-		if classifierType == "svm_1":
-			clf = svm.SVC(kernel='linear', C=1, random_state=0) #   ,class_weight='balanced', probability=True)
-		elif classifierType == "svm_0.5":
-			clf = svm.SVC(kernel='linear', C=0.5, random_state=0)
-		elif classifierType == "svm_0.1":
-			clf = svm.SVC(kernel='linear', C=0.1, random_state=0)
-		elif classifierType == "svm_0.02":
-			clf = svm.SVC(kernel='linear', C=0.02, random_state=0)
-		elif classifierType == "decisionTree":
-			clf = tree.DecisionTreeClassifier(random_state=2018)
+	##scoring = ['precision_macro', 'recall_macro', 'f1_macro']
+	if classifierType == "svm_1":
+		clf = svm.SVC(kernel='linear', C=1, random_state=0) #   ,class_weight='balanced', probability=True)
+	elif classifierType == "svm_0.5":
+		clf = svm.SVC(kernel='linear', C=0.5, random_state=0)
+	elif classifierType == "svm_0.1":
+		clf = svm.SVC(kernel='linear', C=0.1, random_state=0)
+	elif classifierType == "svm_0.02":
+		clf = svm.SVC(kernel='linear', C=0.02, random_state=0)
+	elif classifierType == "decisionTree":
+		clf = tree.DecisionTreeClassifier(random_state=2018)
 		
-		clf = clf.fit(df_TrainSet[features], df_TrainSet["outcome"])
+	clf = clf.fit(df_TrainSet[features], df_TrainSet["outcome"])
+	
+	tree.plot_tree(clf,feature_names=features,class_names=clf.classes_)
+	plt.show()
 		
-
-		tree.plot_tree(clf,feature_names=features,class_names=clf.classes_)
-		plt.show()
+	y_true = df_TestSet["esito"].tolist()
+	y_pred = clf.predict(df_TestSet[features]).tolist()
 		
-		y_true = df_TestSet["esito"].tolist()
-		y_pred = clf.predict(df_TestSet[features]).tolist()
+	print (classification_report(y_true, y_pred,output_dict=True))
 		
-		print (classification_report(y_true, y_pred,output_dict=True))
-		
-		if mylib.isDivergent(y_pred):
-			return {"divergent": True}
+	if isDivergent(y_pred):
+		return {"divergent": True}
 			
-		return {
-				"report": classification_report(y_true, y_pred,output_dict=True),
-				"y_pred": y_pred,
-				"divergent": False
-				}
+	return {
+		"report": classification_report(y_true, y_pred,output_dict=True),
+		"y_pred": y_pred,
+		"divergent": False
+	}
 
 df = pd.read_csv(inputFile)
 
